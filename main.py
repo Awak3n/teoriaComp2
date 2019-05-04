1a6958ae9a550254604329ae2f3d75eb44c1e599
Êndril Castilho da Silveira <endril.n@hotmail.com>
2019-05-04 13:44:43 -0300

    .

---
+++ main.py
@@ -0,0 +1,13 @@
+terminais = ['A','B','C','D','E','F','G','H','I','J']
+nao_terminais = ['a','b','c','d','e','f','g','h','i','j']
+
+class Simbolo(object):
+  #A saida e a entrada será um vetor simbolos
+  def __init__(self, valor):
+      self.valor = valor
+      if(valor)
+
+def main():
+    pass
+
+main()
